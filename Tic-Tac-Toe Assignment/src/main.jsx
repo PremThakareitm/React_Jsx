b4303006d50c1604a4ead6b1675271e26e9f9d2f
Prem Thakare <142775321+PremThakareitm@users.noreply.github.com>
2024-06-12 18:21:13 +0530

    Add files via upload

---
+++ Tic-Tac-Toe Assignment/src/main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import Game from "./App.jsx";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
